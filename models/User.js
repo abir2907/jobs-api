e4844ab9b1c6313aefaee9749002a31c9d705188
Abir Chodha <abirchodha1@gmail.com>
2025-03-09 04:59:52 +0530

    Use middleware for generating hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { required } = require("joi");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = mongoose.Schema({
   name: {
@@ -22,6 +23,11 @@ const UserSchema = mongoose.Schema({
     required: [true, "Please provide password"],
     minLength: 6,
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("User", UserSchema);
