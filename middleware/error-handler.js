67aba24f0ed282ebd05c79d90020e8bd3f67d944
Abir Chodha <abirchodha1@gmail.com>
2025-10-05 01:00:03 +0530

    Add error handling for validation errors during register

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -11,6 +11,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     )} field, please choose another value`;
     customError.statusCode = 400;
   }
+
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+    customError.statusCode = 400;
+  }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
