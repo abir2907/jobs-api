e2126fbf54c0fb594403d872efea80aeeb7fd310
Abir Chodha <abirchodha1@gmail.com>
2025-10-07 17:45:34 +0530

    Add security packages

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -15,9 +22,20 @@ const jobsRouter = require("./routes/jobs");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const { applyTimestamps } = require("./models/Job");
 
-app.use(express.json());
 // extra packages
+app.use(express.json());
+app.set("trust proxy", 1 /* number of proxies between user and server */);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
